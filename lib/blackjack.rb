5d93c9b3757949e529696822e3e77973806b8f9b
Alec Hall <alecnhall@gmail.com>
2019-08-08 01:30:36 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -35,9 +35,9 @@ def hit?(card_total)
   get_user_input
   return card_total
   if get_user_input == 'h'
-    return card_total += deal_card
+    card_total += deal_card
   elsif get_user_input == 's'
-    return card_total
+    card_total
   else
     invalid_command
   end
